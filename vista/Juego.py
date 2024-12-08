eea9869c84a14a3af03475df7fca50009eb4a253
Mer7z <mer7zy@hotmail.com>
2024-12-08 14:50:46 -0500

    Estrellas y evento de keypress

--- vista/Juego.py
+++ vista/Juego.py
@@ -1,11 +1,28 @@
 from tkinter import *
 import tkinter as tk
+import random
 
 
 class Juego():
     def __init__(self, root, jugador):
         self.root = root
         self.jugador = jugador
+
+        self.root.bind("<KeyPress>", self.presionarTecla)
         
         self.canvas = Canvas(self.root, bg="black")
-        self.canvas.place(x=0, y=0, relheight=1, relwidth=1)
+        self.canvas.place(x=0, y=0, relheight=1, relwidth=1) #ventana es 600x500
+
+        self.dibujar_estrellas()
+
+    def presionarTecla(self, event):
+        print(event.keysym)
+
+    def dibujar_estrellas(self):
+        self.canvas.delete("estrellas")
+        for _ in range(40):
+            x = random.randint(0, 600)
+            y = random.randint(0, 500)
+            t = 2
+
+            self.canvas.create_oval(x, y, x+t, y+t, fill="#adadad", outline="",tags="estrellas")
